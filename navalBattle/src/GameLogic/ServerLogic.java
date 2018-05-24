a80a34936e07a6d76019bd8441ccd4cebbba6cbc
Afonso Jorge Ramos <up201506239@fe.up.pt>
2018-05-24 17:20:49 +0100

    Some touches to Server Logic

--- navalBattle/src/GameLogic/ServerLogic.java
+++ navalBattle/src/GameLogic/ServerLogic.java
@@ -6,14 +6,19 @@ public class ServerLogic {
 	
 	private int height;
 	private int width;
+	private int numPlayers;
 
     /**
      * HashMap saving the id of the boat associated to each userName (user logged)
      */
     private ConcurrentHashMap<String, Integer> usersBoats;
     
-	public boolean createMap() {
-		return true;
+	public void createMap() {
+		for (int i = 0 ; i < numPlayers*4 ; i++) {
+			for (int j = 0 ; j < numPlayers*4 ; j++) {
+				usersBoats.put(i + "+" + j, 0); // Populate with water
+			}
+		}
 	}
 	
 	public int height() {
@@ -35,8 +40,8 @@ public class ServerLogic {
 	}
 	
 	public boolean attack(int col, int row, String position) {
-		if (usersBoats.get(col + "+" + row) != 0) {
-			usersBoats.replace(col + "+" + row, 0);
+		if (usersBoats.get(col + "+" + row) != 0) { // 0 - Water
+			usersBoats.replace(col + "+" + row, 1); // 1 - Destroyed ship
 			return true;
 		}
 		
