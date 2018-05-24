dc12ddeef098990b1db4e7fe034399e2e352e8b0
Afonso Jorge Ramos <up201506239@fe.up.pt>
2018-05-24 16:23:38 +0100

    Server Side Logic Start

--- navalBattle/src/GameLogic/ServerLogic.java
+++ navalBattle/src/GameLogic/ServerLogic.java
@@ -3,9 +3,57 @@ package GameLogic;
 import java.util.concurrent.ConcurrentHashMap;
 
 public class ServerLogic {
+	
+	private int height;
+	private int width;
 
     /**
      * HashMap saving the id of the boat associated to each userName (user logged)
      */
-    ConcurrentHashMap<String, Integer> usersBoats;
+    private ConcurrentHashMap<String, Integer> usersBoats;
+    
+	public boolean createMap() {
+		return true;
+	}
+	
+	public int height() {
+		return height;
+	}
+
+	public int width() {
+		return width;
+	}
+	
+	public int get(int col, int row) {
+		return usersBoats.get(col + "+" + row);
+	}
+	
+	public int getFromId(int col, int row, int id) {
+		if (usersBoats.get(col + "+" + row) == id)
+			return usersBoats.get(col + "+" + row);
+		return 0;
+	}
+	
+	public boolean attack(int col, int row, String position) {
+		if (usersBoats.get(col + "+" + row) != 0) {
+			usersBoats.replace(col + "+" + row, 0);
+			return true;
+		}
+		
+		return false;
+	}
+	
+	public String encodeForPlayer(int id) {
+		StringBuilder sb = new StringBuilder();
+		for(int i = 0; i < height(); i++) {
+			sb.append("[");
+			for(int j = 0; j < width(); j++) {
+				sb.append(getFromId(i,j, id));
+				if(j != width()-1) sb.append(",");
+			}
+			sb.append("]\n");
+		}
+		sb.deleteCharAt(sb.length()-1);
+		return sb.toString();
+	}
 }
