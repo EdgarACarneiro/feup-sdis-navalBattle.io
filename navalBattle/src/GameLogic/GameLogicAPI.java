94196da33c809997745fe03cb3f4e55d0ee24cba
Afonso Jorge Ramos <up201506239@fe.up.pt>
2018-05-25 19:22:46 +0100

    Client

--- navalBattle/src/GameLogic/GameLogicAPI.java
+++ navalBattle/src/GameLogic/GameLogicAPI.java
@@ -1,13 +1,22 @@
 package GameLogic;
 
+import java.util.Date;
+
 public class GameLogicAPI {
 	
 	private int[][] map;
-	private int height;
-	private int width;
+	private int length;
+	private boolean gameOver = false;
 
     // Different behaviours if it is either a Server or a Player
     // Usar design pattern - nao me estou a recordar do nome
+	
+	public static void turn() {
+		Date dNow = new Date( );
+		
+    		  
+		System.out.println("Current Date: " + ft.format(dNow));
+	}
 	
 	public void parseMap(String mapString) {
 						
@@ -15,27 +24,27 @@ public class GameLogicAPI {
 		
 		map = new int[columns.length][columns.length];
 		
+		gameOver = true;
+		
 		for(int i = 0; i < columns.length ; i++) {
 			String[] rows = columns[i].split(",");
 			for(int j = 0; j < rows.length ; j++) {
 				map[i][j] = Integer.parseInt(rows[j]);
+				if (Integer.parseInt(rows[j]) != 0)
+					gameOver = false;
 			}
 		}
 	}
 	
-	public int height() {
-		return height;
-	}
-
-	public int width() {
-		return width;
+	public int getLength() {
+		return length;
 	}
 	
 	public int get(int col, int row) {
 		return map[col][row];
 	}
 	
-	public boolean attack(int col, int row) {
+	public boolean set(int col, int row) {
 		if (map[col][row] == 0)
 			return true;
 		return false;
@@ -43,10 +52,10 @@ public class GameLogicAPI {
 	
 	public String toString() { //Or send move?
 		StringBuilder sb = new StringBuilder();
-		for(int i = 0; i < height(); i++) {
-			for(int j = 0; j < width(); j++) {
+		for(int i = 0; i < getLength(); i++) {
+			for(int j = 0; j < getLength(); j++) {
 				sb.append(get(i,j));
-				if(j != width()-1) sb.append(",");
+				if(j != getLength()-1) sb.append(",");
 			}
 			sb.append("-");
 		}
